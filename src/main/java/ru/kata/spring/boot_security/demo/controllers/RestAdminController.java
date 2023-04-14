8f0fefabc518701c35f1b64bfafd9d8b6ff2c40f
Andrey Rumyantsev <119076724+dronbless@users.noreply.github.com>
2023-04-14 14:21:48 +0300

    imports

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/RestAdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/RestAdminController.java
@@ -3,13 +3,9 @@ package ru.kata.spring.boot_security.demo.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import ru.kata.spring.boot_security.demo.models.User;
-import ru.kata.spring.boot_security.demo.services.RoleService;
 import ru.kata.spring.boot_security.demo.services.UserService;
-
-import java.security.Principal;
 import java.util.List;
 
 
--- src/main/java/ru/kata/spring/boot_security/demo/createTable/Init.java
+++ src/main/java/ru/kata/spring/boot_security/demo/createTable/Init.java
@@ -7,13 +7,11 @@ import ru.kata.spring.boot_security.demo.models.Role;
 import ru.kata.spring.boot_security.demo.models.User;
 import ru.kata.spring.boot_security.demo.repositories.RoleRepository;
 import ru.kata.spring.boot_security.demo.repositories.UserRepository;
-import ru.kata.spring.boot_security.demo.services.RoleService;
-import ru.kata.spring.boot_security.demo.services.UserService;
+
 
 import javax.annotation.PostConstruct;
 import javax.transaction.Transactional;
-import java.util.HashSet;
-import java.util.Set;
+
 
 @Component
 public class Init {
